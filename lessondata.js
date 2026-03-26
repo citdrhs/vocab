c031975aa960cebedfcf1b5fb87e84e50748a1ce
EpicMansa <hcps-bhagodis@henricostudents.org>
2026-03-26 10:11:56 -0400

    Update lessondata.js

--- lessondata.js
+++ lessondata.js
@@ -3,7 +3,125 @@ var lesson_data = { //all of the data for each of the lessons
         "words" : {}
     },
     "2" : {
-        "words" : {}
+        "words" : {"deluge" : { //the word
+                "ps" : "noun", //part of speech
+                "def" : "a flood; an overwhelming rush", //defenition
+                "ex" : "The new amusement park experienced a ____ of visitors on opening day.", //example sentence, _ x4 is where the word goes
+                "ant" : ["drought", "dearth"], //antonyms
+                "syn" : ["inundation", "surge"] //synonyms
+            },
+
+            "catholic" : {
+                "ps" : "adjective",
+                "def" : "universal; wide-ranging",
+                "ex" : "His ____ interests made him quite knowledgeable in may subjects",
+                "ant" : ["provincial", "limited", "parochial"],
+                "syn" : ["broad"]
+            },
+
+            "eerie" : {
+                "ps" : "adjective",
+                "def" : "weird; mysterious; strange and frightening",
+                "ex" : "No one accepted the dare to stay in the ____ old mansion for one night.",
+                "ant" : ["common", "ordinary"],
+                "syn" : ["creepy", "sinister"]
+            },
+
+            "martial" : {
+                "ps" : "adjective",
+                "def" : "warlike; relating to the military",
+                "ex" : "A state of ____ law was declared in the small country in the weeks following the overthrow of the government.",
+                "ant" : [],
+                "syn" : []
+            },
+
+            "anthropomorphic" : {
+                "ps" : "adjective",
+                "def" : "attributing human characteristics or qualities to objects, animals, or gods",
+                "ex" : "____ stories might feature pigs and rabbits walking upright wearing clothes, and speaking to each other in human languages.",
+                "ant" : [],
+                "syn" : []
+            },
+
+            "beneficiary" : {
+                "ps" : "noun",
+                "def" : "one who receives benefits",
+                "ex" : "John was the sole ____ of his Uncle Martin’s vast estate.",
+                "ant" : [],
+                "syn" : ["recipient", "heir"]
+            },
+
+            "careen" : {
+                "ps" : "verb",
+                "def" : "to swerve or lurch from side to side while in motion",
+                "ex" : "The torrential winds caused the ship to ____ violently.",
+                "ant" : [],
+                "syn" : ["tilt"]
+            },
+
+            "aplomb" : {
+                "ps" : "noun",
+                "def" : "self-confidence",
+                "ex" : "The ____ of the young dancer astonished the veterans of the troupe.",
+                "ant" : ["awkwardness"],
+                "syn" : ["assurance", "poise"]
+            },
+
+            "guile" : {
+                "ps" : "noun",
+                "def" : "slyness and cunning in dealing with others",
+                "ex" : "Brad’s ____ contributed to his wealth, but it also created enemies.",
+                "ant" : ["honesty"],
+                "syn" : ["craftiness", "astuteness"]
+            },
+
+            "modicum" : {
+                "ps" : "noun",
+                "def" : "a small amount",
+                "ex" : "A sudden shower gave us a ____ of relief from the heat and humidity.",
+                "ant" : ["abundance"],
+                "syn" : ["bit"]
+            },
+
+            "rancid" : {
+                "ps" : "adjective",
+                "def" : "having a bad taste ",
+                "ex" : "The bitter fight over child custody left a ____ taste in both lawyers' mouths.",
+                "ant" : ["fresh"],
+                "syn" : ["rotten", "repulsive"]
+            },
+
+            "havoc" : {
+                "ps" : "noun",
+                "def" : "great destruction; chaos",
+                "ex" : "The commandos wreaked ____ throughout the area when they infiltrated the secret base.",
+                "ant" : ["order"],
+                "syn" : ["mayhem", "disorder"]
+            },
+
+            "pall" : {
+                "ps" : "noun",
+                "def" : "something that covers or conceals",
+                "ex" : "A ____ of gloom descended over the crowd.",
+                "ant" : [],
+                "syn" : ["shroud"]
+            },
+
+            "languish" : {
+                "ps" : "verb",
+                "def" : "to become weak or feeble; to lose strength",
+                "ex" : "I ____ed at the thought of all the work that still needed to be done",
+                "ant" : ["thrive"],
+                "syn" : ["wither", "fade"]
+            },
+
+            "fester" : {
+                "ps" : "verb",
+                "def" : "to grow embittered over time; to rot",
+                "ex" : "If allowed to ____, dislike can turn into bitter hatred.",
+                "ant" : [],
+                "syn" : ["aggravated", "worsen"]
+            }
     },
     "12" : { //lesson #
         "words" : { //all of the words
